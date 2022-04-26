6420377f4e6a12916ad0665db7d12f6cb1cb2823
Joyrun <joyrun@JoyrundeMacBook-Pro.local>
2022-04-26 17:32:20 +0800

    First Commit

---
+++ lib/main.dart
@@ -0,0 +1,22 @@
+import 'package:android_util/page/home_page.dart';
+import 'package:flutter/material.dart';
+
+void main() {
+  runApp(const MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  const MyApp({Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      title: 'AndroidADBUtil',
+      debugShowCheckedModeBanner: false,
+      theme: ThemeData(
+        primarySwatch: Colors.blue,
+      ),
+      home: const HomePage(),
+    );
+  }
+}
